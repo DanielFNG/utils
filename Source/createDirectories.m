adc8f8a4cfe7959b90df305db745c7a5b282564d
Daniel Gordon <DanielFNG@users.noreply.github.com>
2019-01-30 14:43:28 +0100

    Create common tools directory
    This minimal repo is for tools which are shared between multiple repos
    (opensim-matlab, echo, etc) but are not necessarily intrinsically
    related to any of them.

---
+++ Source/createDirectories.m
@@ -0,0 +1,14 @@
+function createDirectories(dir_struct)
+% Create directories given a stucture holding directory paths.
+% 
+% Input is a structure formed as so:
+%   dir_struct.dir1 = 'apple'
+%   dir_struct.dir2 = 'banana'
+% This function would then create the directories 'apple' and 'banana'.
+
+dirs = fieldnames(dir_struct);
+for i=1:length(dirs)
+    mkdir(dirs(i));
+end
+
+end

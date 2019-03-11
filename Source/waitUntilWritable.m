866090aaf78dbbdf19bb7e436d3683d3c37ec22c
Daniel Gordon <daniel.gordon@ed.ac.uk>
2019-03-11 11:37:13 +0000

    Update waitUntilWritable.m

--- Source/waitUntilWritable.m
+++ Source/waitUntilWritable.m
@@ -1,9 +1,15 @@
 function waitUntilWritable(file, time)
-% Check if a file is writable. Pause for a given time if not. Repeat. 
+% Check if a file exists & is writable. Pause for a given time if not. Repeat. 
+
+while ~(exist(file, 'file') == 2)
+    fprintf('Waiting for file to exist.\n');
+    pause(time);
+end
 
 [~, values] = fileattrib(file);
 
 while ~values.UserWrite
+    fprintf('Waiting for file to be writable.\n');
     pause(time);
 end
 

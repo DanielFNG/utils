b316c91313b1485e76023a1815ecf83c5fa005f9
Daniel Gordon <daniel.gordon@ed.ac.uk>
2020-05-11 14:43:00 +0100

    Update dirNoDots.m

--- Source/dirNoDots.m
+++ Source/dirNoDots.m
@@ -1,11 +1,14 @@
 function [n_files, files] = dirNoDots(directory)
-% Return number of files & filenames from a directory. 
 
-d = dir(directory);
-d = d(~ismember({d.name}, {'.', '..'}));
+    % Return number of files & filenames from a directory. 
 
-n_files = length(d);
-files = cell(1, n_files);
-for i=1:n_files
-    files{i} = [directory filesep d(i).name];
+    d = dir(directory);
+    d = d(~ismember({d.name}, {'.', '..'}));
+
+    n_files = length(d);
+    files = cell(1, n_files);
+    for i=1:n_files
+        files{i} = [directory filesep d(i).name];
+    end
+
 end
